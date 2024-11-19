1133a7e831c9ab81e99b9c9ad382e1501b12e459
Andres <aaplv2@gmail.com>
2024-11-18 23:45:34 -0300

    cambio orden index

--- api/index.js
+++ api/index.js
@@ -13,8 +13,6 @@ const eventsRoute = require("./routes/events.js");
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(express.json());
-
 // app.use(cors());
 
 // app.options("*", cors());
@@ -33,6 +31,8 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+
+app.use(express.json());
 
 app.use(express.static("/api/images"));
 
